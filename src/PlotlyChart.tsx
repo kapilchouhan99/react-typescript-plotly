81c64846aace6026ccc8833d006d734035bae3e1
Kapil Chouhan <kpl.chouhan99@gmail.com>
2023-06-20 20:35:42 +0530

    Update chart style

--- src/PlotlyChart.tsx
+++ src/PlotlyChart.tsx
@@ -1,115 +1,104 @@
 import React, { useEffect } from 'react';
 import Plot from 'plotly.js-basic-dist';
-import {TypeUser} from "./interface/User";
+import { TypeUser } from "./interface/User";
 
 type PlotlyChartProps = {
-  // data: Plotly.Data[];
   data: TypeUser[];
-  // layout: Partial<Plotly.Layout>;
 };
 
 const PlotlyChart: React.FC<PlotlyChartProps> = ({ data }) => {
-  const topic = data.map((item) => item.topic );
-  const score = data.map((item) => item.score );
+  console.log('=>', data);
+  
+  const plotlyHeightConfig: { [key: string]: number; } = {'1': 260, '2': 320, '3': 370, '4': 430, '5': 500};
+
+
+  const topic = data.map(item => item.topic);
+  const score = data.map(item => item.score);
+  const shapesData = [0, 20, 40, 60, 80];
+
+  const getShapes = (): Partial<Plotly.Shape>[] => {
+    return shapesData.map((value) => ({
+      type: 'line',
+      x0: value,
+      y0: 0,
+      x1: value,
+      yref: 'paper',
+      y1: 1,
+      opacity: 0.5,
+      line: {
+        color: 'grey',
+        width: 1.5,
+        dash: 'dot',
+      },
+    }));
+  };
+
+  const shapes = getShapes();
 
   const layout: Partial<Plotly.Layout> = {
     title: {
-      text:'Security risk score by scan',
+      text: 'Security risk score by scan',
       font: {
         size: 24,
-        color: "black"
+        color: 'black',
       },
-      xref: 'paper',
       x: 0,
     },
     xaxis: {
-      zeroline: false
+      autorange: true,
+      zeroline: false,
+      showgrid: false,
+      showline: false,
     },
     yaxis: {
       showline: false,
-      automargin: true
+      automargin: true,
+      autorange: true,
     },
-    
-    shapes: [{
-        type: 'line',
-        x0: 0,
-        y0: 0,
-        x1: 0,
-        yref: 'paper',
-        y1: 1,
-        opacity: 0.5,
-        line: {
-          color: 'grey',
-          width: 1.5,
-          dash: 'dot'
-        }
-      },
-      {
-        type: 'line',
-        x0: 20,
-        y0: 0,
-        x1: 20,
-        yref: 'paper',
-        y1: 1,
-        opacity: 0.5,
-        line: {
-          color: 'grey',
-          width: 1.5,
-          dash: 'dot'
-        }
-      },
-      {
-        type: 'line',
-        x0: 40,
-        y0: 0,
-        x1: 40,
-        yref: 'paper',
-        y1: 1,
-        opacity: 0.5,
-        line: {
-          color: 'grey',
-          width: 1.5,
-          dash: 'dot'
-        }
-      },
-      {
-        type: 'line',
-        x0: 60,
-        y0: 0,
-        x1: 60,
-        yref: 'paper',
-        y1: 1,
-        opacity: 0.5,
-        line: {
-          color: 'grey',
-          width: 1.5,
-          dash: 'dot'
-        }
-      },
-      {
-        type: 'line',
-        x0: 80,
-        y0: 0,
-        x1: 80,
-        yref: 'paper',
-        y1: 1,
-        opacity: 0.5,
-        line: {
-          color: 'grey',
-          width: 1.5,
-          dash: 'dot'
-        }
-      }],
+    shapes: shapes,
     margin: {
-      l: 50, r: 50, b: 100, t: 100
-    } 
+      l: 50,
+      r: 50,
+      b: 100,
+      t: 100,
+    },
+    showlegend: false,
+    annotations: [],
+    barmode: 'stack',
+    height: plotlyHeightConfig[data.length.toString()],
+    bargap: .7,
   };
-  const d: Plotly.Data[]  = [{x: score, y: topic, type: 'bar', orientation: 'h'}];
+
+  const d: Plotly.Data[] = [
+    {
+      x: score,
+      y: topic,
+      type: 'bar',
+      orientation: 'h',
+      mode: 'markers',
+      marker: {
+        size: 1,
+        color: '#2084ff',
+      },
+    },
+    {
+      x: score,
+      y: topic,
+      type: 'scatter',
+      orientation: 'h',
+      mode: 'markers',
+      marker: {
+        size: 17,
+        color: '#2084ff',
+      },
+      hoverinfo: 'none',
+    }
+  ];
 
   useEffect(() => {
     Plot.newPlot('chart', d, layout);
     return () => {
-        Plot.purge('chart');
+      Plot.purge('chart');
     };
   }, [data, layout]);
 
